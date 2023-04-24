ce0f655b62e340f799d1792700f8381174665626
Shamveel Ahammed <shamveelahammed@gmail.com>
2023-04-24 01:13:44 +0100

    added safearea to limit overflow

--- my_app/lib/login.dart
+++ my_app/lib/login.dart
@@ -12,36 +12,39 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
+    return SafeArea(
+      bottom: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                ),
               ),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
               ),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              child: Text('Login'),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/login');
-                // TODO: Login the user
-              },
-            ),
-          ],
+              ElevatedButton(
+                child: Text('Login'),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/login');
+                  // TODO: Login the user
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
